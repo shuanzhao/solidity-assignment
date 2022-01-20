3f3958079f7ec03dd3be94d6f27f749f026695c1
Shuan Zhao <erdayegauss@163.com>
2022-01-20 09:54:47 +0800

    update a bit

--- cr.js
+++ cr.js
@@ -14,48 +14,57 @@ const voting = async () =>{
 
 let accounts = await web3.eth.getAccounts();
 account =accounts[0]
+		option = {from: account,gas: 300000,gasPrice:1200000000}
 
 //get the endTime
-let time_end =await  CommitReveal.methods.commitPhaseEndTime().call({from: account}).then(function(endtime){
-	return endtime
-});
+	let 	time_end =await  CommitReveal.methods.commitPhaseEndTime().call({from: account}).then(function(endtime){
+		return endtime
+	});
 
 
- 	while(Date.now()/1000 < time_end) {
+			console.log("The time is not up, you can still vote")
+       	        	var vote = readlineSync.question('input your vote: ');
 
-        var vote = readlineSync.question('The time is not up, you can still vote,  What is your vote? ');
-        const commit = web3.utils.soliditySha3(vote);
+			if (vote != ""){               
+                	const commit = web3.utils.soliditySha3(vote);
+                	var commitVote =  CommitReveal.methods.commitVote(commit).send(option).then(function(result){
+                        	console.log("the commit result is:",result);
+                	});
+			}
+        
+	let commitsArray =await CommitReveal.methods.getVoteCommitsArray().call({from: account})
 
-	var commitVote =  CommitReveal.methods.commitVote(commit).call({from: account}).then(function(result){
-//        console.log("the commit result is:",result);
-        });
+	await CommitReveal.methods.voteStatuses(commitsArray[0]).call({from: account}).then(function(result){
+                        console.log("the vote status is:",result);
+                });
 
-	var commitsArray = await CommitReveal.methods.getVoteCommitsArray().call({from: account}).then(function(result){
-//        console.log("the commitsArray is", result)
-	})
+        var numberOfVotesCast = await CommitReveal.methods.numberOfVotesCast().call({from: account})
+	console.log("the numberOfVotesCast is:", numberOfVotesCast)
+	var i=0
+
+	while(i<numberOfVotesCast){
+	        console.log("we are waiting for more votes to be casted")
+       		var vote_given = readlineSync.question('please input your given vote, if you have not done it yet');
+	        const commit_given = web3.utils.soliditySha3(vote_given);
+
+		let voteStatus = await CommitReveal.methods.voteStatuses(commit_given).call({from: account})
+		
+		if(voteStatus != "Revealed"){
+
+        	var revealVote = await  CommitReveal.methods.revealVote(vote_given,commit_given).send(option).then(function(result){
+        	console.log("the reveal vote results are:",result);
+        	});
+
+		}
+		i=i+1	
 	}
 
-// check the numberOfVotesCast, and get the winer when the all the votes are casted
-        var numberOfVotesCast =  CommitReveal.methods.numberOfVotesCast().call({from: account}).then(function(number){
-	while(number < total_votes){
-	
-	// prompt for revealing vote
-        console.log("we are waiting for more votes to be casted")
-        var vote_given = readlineSync.question('please input your given vote, if you have not done it yet');
-        const commit_given = web3.utils.soliditySha3(vote_given);
-
-	// revealing Vote
-        var revealVote =   CommitReveal.methods.revealVote(vote_given,commit_given).call({from: account}).then(function(result)        {
-        console.log("the reveal vote results are:",result);
-        });
-
-	}
 	
 	console.log("Time to declear the winner,hold your breath")
 	var winner = CommitReveal.methods.getWinner().call({from: account}).then(function(winner){
         console.log("Congrats, the winner is:", winner);
-        })
-	
         });
+	
+
 }
 voting()
